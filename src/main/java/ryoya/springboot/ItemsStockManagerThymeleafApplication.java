c8e8bbc45dd8319de102afbb79397ade4e2731d4
ryoya katagiri <ryoya3091@yahoo.co.jp>
2018-06-30 17:46:02 +0900

    First commit

---
+++ src/main/java/ryoya/springboot/ItemsStockManagerThymeleafApplication.java
@@ -0,0 +1,12 @@
+package ryoya.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsStockManagerThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsStockManagerThymeleafApplication.class, args);
+	}
+}
